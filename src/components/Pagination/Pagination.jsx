e403a8241bc30d7e297cb9d69bbaeee515399d79
Saroj Maurya <sarojsanmaurya07@gmail.com>
2025-02-18 16:37:57 +0530

    Pagination added

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -24,7 +24,10 @@ const Pagination = ({ setCurrentPage, currentPage }) => {
         <img src={leftarrow} alt="Prev" />
       </button>
 
-      <h1 className="page-no">{currentPage}</h1>
+      <button className="button" onClick={()=>setCurrentPage(currentPage+1)}>{currentPage}</button>
+      <button className="button" onClick={()=>setCurrentPage(currentPage+2)}>{currentPage+1}</button>
+      <button className="button" onClick={()=>setCurrentPage(currentPage+3)}>{currentPage+2}</button>
+      <button className="button" onClick={()=>setCurrentPage(currentPage+4)}>{currentPage+3}</button>
 
       <button className="next-button" onClick={() => nextPage(currentPage)}>
         <img src={rightarrow} alt="Next" />
